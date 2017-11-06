5f8e8de4eb4d0fe0115762b9295791f36f51ac57
suhaga15 <suhaga15@gmail.com>
2017-11-06 22:57:06 +0530

    Delete config.php

--- config.php
+++
@@ -1,7 +0,0 @@
-<?php
-     $db_con = mysql_connect("localhost","root","abc123");
-     if (!$db_con){
-          die('Could not connect: ' . mysql_error());
-     }
-     mysql_select_db("my_data", $db_con);
-?>
